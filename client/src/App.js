28860b528729a932981d40ac57c001ef6c47d4a4
Natalie Salemme <NatalieSalemme@gmail.com>
2019-01-21 01:35:34 -0800

    adding redux store and root reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Login from './components/auth/Login';
@@ -8,9 +9,11 @@ import Register from './components/auth/Register';
 
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <BrowserRouter>
       <div className="App">
         <Navbar />
@@ -19,6 +22,7 @@ class App extends Component {
         <Footer />
       </div>
     </BrowserRouter>
+  </Provider>
     );
   }
 }
