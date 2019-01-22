55bf8db147bc4e01a367afcbfa17b9f6d59ecc01
Natalie Salemme <NatalieSalemme@gmail.com>
2019-01-22 14:06:10 -0800

    adding profile header

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import './App.css';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 //Check for token
 if (localStorage.jwtToken) {
   //Set auth token header auth
@@ -48,6 +49,7 @@ class App extends Component {
             <Route exact path="/" component={Register} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/profiles" component={Profiles} />
+            <Route exact path="/profile/:handle" component={Profile} />
             <Switch>
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
