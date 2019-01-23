ae5c21443e98612d00d23ce959a149ad15e03343
Natalie Salemme <NatalieSalemme@gmail.com>
2019-01-22 16:52:00 -0800

    adding Post page ui

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -6,6 +6,7 @@ import {
   GET_POSTS,
   POST_LOADING,
   DELETE_POST,
+  GET_POST,
 } from './types';
 
 //Add Post
@@ -45,6 +46,24 @@ export const getPosts = () => dispatch => {
     );
 };
 
+//Get Post
+export const getPost = id => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data,
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POST,
+        payload: null,
+      })
+    );
+};
 //Delete post
 
 export const deletePost = id => dispatch => {
