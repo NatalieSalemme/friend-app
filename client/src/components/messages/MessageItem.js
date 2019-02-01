cbf7bbe62742bbbfc5e57d6c070364f74c7a1d82
Natalie Salemme <NatalieSalemme@gmail.com>
2019-01-31 23:04:39 -0800

    adding reply and delete button to each messageItem

--- client/src/components/messages/MessageItem.js
+++ client/src/components/messages/MessageItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import moment from 'moment';
+import { Link } from 'react-router-dom';
 
 class MessageItem extends React.Component {
   render() {
@@ -8,13 +9,44 @@ class MessageItem extends React.Component {
     let messageDate = message.date.toString();
     // let formattedDate = moment({ messageDate }, 'MM/DD/YYYY');
     return (
-      <div
-        style={{ border: '2px solid black' }}
-        className="my-2 container card"
-      >
-        <div>Text: {message.message}</div>
-        <div>{moment(messageDate).format('MM/DD/YYYY LT')}</div>
-        <div>From: {message.from}</div>
+      <div className="card card-body mb-3 col-md-8 mx-auto">
+        <div className="row">
+          <div className="col-md-2">
+            {/* <a href="profile.html">
+                <img
+                  className="rounded-circle d-none d-md-block"
+                  src={comment.avatar}
+                  alt=""
+                />
+              </a> */}
+
+            <br />
+            <div className="text-center">
+              <img
+                className=" mr-4"
+                style={{ width: '70px', height: '70px' }}
+                src={require('../images/rose.jpg')}
+                alt="avatar"
+              />
+              <h4>{message.from}</h4>
+            </div>
+          </div>
+          <div className="col-md-10">
+            <div className="text-right">
+              {moment(messageDate).format('MM/DD/YYYY LT')}
+            </div>
+            <p className="lead text-left">{message.message}</p>
+
+            <div class="d-flex justify-content-between">
+              <Link to="/messages/reply/id" className="btn btn-secondary">
+                Reply
+              </Link>
+              <button type="button" className="btn btn-danger">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
