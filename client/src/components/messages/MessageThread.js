02c598f6b505346dfefd25f0b1b18cfa60d68fc8
Natalie Salemme <NatalieSalemme@gmail.com>
2019-02-06 23:10:36 -0800

    adding reply form on messageThread Item

--- client/src/components/messages/MessageThread.js
+++ client/src/components/messages/MessageThread.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getMessageThread } from '../../actions/messageActions';
 import Spinner from '../common/Spinner';
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import MessageThreadItem from './MessageThreadItem';
 
 class MessageThread extends Component {
@@ -23,7 +24,29 @@ class MessageThread extends Component {
       <div>
         <h1>Message Thread</h1>
         <div>{messageContent}</div>
-        <button>Reply</button>
+
+        <div className="post-form mb-3 col-md-6 mx-auto">
+          <div className="card card-info">
+            <div className="card-header bg-info text-white">Reply</div>
+            <div className="card-body">
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <TextAreaFieldGroup
+                    placeholder="Reply to post"
+                    name="text"
+                    // avatar={this.state.avatar}
+                    // value={this.state.text}
+                    onChange={this.onChange}
+                    // error={errors.text}
+                  />
+                </div>
+                <button type="submit" className="btn btn-dark">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
