d534ca675272275b794a585e599c31c78eaf20fd
Natalie Salemme <NatalieSalemme@gmail.com>
2019-03-15 18:01:38 -0700

    adding config_keys_dev to .gitignore

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
-// if (process.env.NODE_ENV === 'production') {
-//   module.exports = require('./keys_prod');
-// } else {
-//   module.exports = require('./keys_dev');
-// }
-module.exports = {
-  mongoURI: 'mongodb://tatalie:tatalie2@ds161794.mlab.com:61794/friend-search',
-  secretOrKey: 'gajsfkjas',
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
+// module.exports = {
+//   mongoURI: 'mongodb://tatalie:tatalie2@ds161794.mlab.com:61794/friend-search',
+//   secretOrKey: 'gajsfkjas',
+// };
