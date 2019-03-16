5eac0734d0d9f0f2ed5832e4b9977f234141c463
Natalie Salemme <NatalieSalemme@gmail.com>
2019-03-15 18:20:42 -0700

    adding keys_dev back

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,3 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   module.exports = require('./keys_dev');
 }
-// module.exports = {
-//   mongoURI: 'mongodb://tatalie:tatalie2@ds161794.mlab.com:61794/friend-search',
-//   secretOrKey: 'gajsfkjas',
-// };
